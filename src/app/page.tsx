12ebb064f83e15a0f3b2463eed8e9549bc7be9b4
Kislay - Parallel Minds <kislay.kislay@parallel-minds.com>
2025-09-07 03:10:48 +0530

    added sandbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,27 @@
-import { Suspense } from "react";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { getQueryClient, trpc } from "@/trpc/servers";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import Image from "next/image";
-import Client from "./client";
+"use client";
 
-export default  async function Home() {
-  const queeryClient = getQueryClient();
-  void queeryClient.prefetchQuery(trpc.createAI.queryOptions({ prompt: "world" }));
-  
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function Home() {
+  const [value, setValue] = useState("");
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background task invoked!");
+    }
+  }));
+
   return (
-    <HydrationBoundary state={dehydrate(queeryClient)}>
-     <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-     </Suspense>
-    </HydrationBoundary>
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
+        Invoke Background Task
+      </Button>
+    </div>
   );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,24 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/inngest/client';
+
 export const appRouter = createTRPCRouter({
+  invoke: baseProcedure
+    .input(
+      z.object({
+        value: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: 'test/hello.world',
+        data: {
+          value: input.value,
+        },
+      });
+      return { ok: "Success" };
+    }),
+
   createAI: baseProcedure
     .input(
       z.object({
